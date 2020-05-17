df7449199e314e343976f74bf88884f1989066fb
MilindRajpoot <rajpootmilind@gmail.com>
2020-05-17 16:15:36 +1000

    Service files

--- aflSite/src/app/app.module.ts
+++ aflSite/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'hero', component: HeroSectionComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
