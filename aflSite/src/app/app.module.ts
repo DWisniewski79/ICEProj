fcfb5994f5e8c42cfe0ce98fb333c556778a6200
DAVID WISNIEWSKI <17917777@students.latrobe.edu.au>
2020-06-03 00:47:06 +1000

    Adding for some form of functionality

--- aflSite/src/app/app.module.ts
+++ aflSite/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TeamStatsComponent } from './team-stats/team-stats.component';
+import{CompleteGameResultsService} from './complete-game-results.service';
+import{HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamStatsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CompleteGameResultsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ aflSite/src/app/team-stats/team-stats.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, Input} from '@angular/core';
+import{CompleteGameResultsService} from '../complete-game-results.service';
+import { HttpClient} from '@angular/common/http';
+import{Game} from '../game';
+import { element } from 'protractor';
+
+@Component({
+  selector: 'app-team-stats',
+  templateUrl: './team-stats.component.html',
+  styleUrls: ['./team-stats.component.css']
+})
+export class TeamStatsComponent implements OnInit {
+
+  games: Game[];
+
+  roundSelect: number = 1;
+  
+  constructor(private gameService: CompleteGameResultsService) 
+  {
+      
+  }
+
+  ngOnInit(): void 
+  {
+    this.getGames();
+    
+  }
+
+  getGames(): void
+  {
+    this.gameService.getCompleteGameResult().subscribe(temp=> {this.games = temp
+    
+      
+    var tempArr = [];
+
+
+    temp.forEach(element => {
+      if(element.round == this.setRound(this.roundSelect)) tempArr.push(element);
+    });
+
+
+
+    this.games = tempArr;
+    console.log(this.games);
+  
+    });
+
+  }
+
+
+  setRound(x): void
+  {
+    console.log("the selected value is " + x);
+    this.roundSelect = x;
+    console.log(this.roundSelect);
+  }
+ 
+
+}
