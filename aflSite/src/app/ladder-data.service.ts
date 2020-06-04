123e15f08b79d82084be8846571e476e0e9eab7b
BlairMut <62121940+BlairMut@users.noreply.github.com>
2020-06-05 00:56:43 +1000

    Saving changes

--- aflSite/src/app/ladder-data.service.ts
+++ aflSite/src/app/ladder-data.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {ladder} from './ladder';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +11,26 @@ export class LadderDataService {
 
   constructor(private http: HttpClient) { }
 
-  getLadderData()
+  getLadder():Observable<ladder[]>
   {
-    return this.http.get('https://api.squiggle.com.au/?q=standings;year=2019;round=20');
+    return this.http.get<ladder[]>('https://api.squiggle.com.au/?q=standings;year=2019;round=20').pipe(
+      map((data: any) => data.ladder.map((item: any) => new ladder(
+        item.played,
+        item.pts,
+        item.goals_against,
+        item.wins,
+        item.behinds_for,
+        item.aginst,
+        item.losses,
+        item.name,
+        item.id,
+        item.draws,
+        item.for,
+        item.goals_for,
+        item.rank,
+        item.behinds_against,
+        item.percentage
+      )))
+    );
   }
 }
--- aflSite/src/app/team-data.service.ts
+++ aflSite/src/app/team-data.service.ts
@@ -4,6 +4,7 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class TeamDataService {
+  getLadder: any;
 
   constructor(private http: HttpClient) { }
 
